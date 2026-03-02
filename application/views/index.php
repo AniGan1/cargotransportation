3075a0e86293f9241a556f1174aa13be261ed6f2
student8 <ntti@yandex.ru>
2026-03-02 15:38:45 +0300

    Main page

--- application/views/index.php
+++ application/views/index.php
@@ -1,9 +1,52 @@
 <main>
-    <div class="row">
-        <div class="col-1"></div>
-        <div class="col-10">
-            <h1>Главная</h1>
+    <div class="container">
+        <div class="card text-bg-dark">
+            <img src="/img/gruz1.jpg" class="card-img" alt="...">
+            <div class="card-img-overlay">
+                <h5 class="card-title text-light">Грузоперевозки по<br>
+                    России, СНГ и Азии</h5>
+                <p class="card-text text-light">
+                    Авто перевозки. Сборные грузы. Ведущая транспортная компания "КаргоГруз" - это уверенность в
+                    оперативной и<br>
+                    качественной доставке вашего груза</p>
+            </div>
         </div>
-        <div class="col-1"></div>
+        <br>
+        <a href="#" type="button" class="btn btn-primary btn-lg">Оставить заявку на доставку груза</a>
+    </div>
+    <br>
+    <div class="container">
+        <h1 class="text-center">Наши услуги</h1>
+        <div class="row row-cols-1 row-cols-md-3 g-4">
+  <div class="col">
+    <div class="card h-100">
+      <img src="..." class="card-img-top" alt="...">
+      <div class="card-body">
+        <h5 class="card-title">Заголовок карточки</h5>
+        <button type="button" class="btn btn-primary">Узнать больше</button>
+      </div>
+    </div>
+  </div>
+  <div class="col">
+    <div class="card h-100">
+      <img src="..." class="card-img-top" alt="...">
+      <div class="card-body">
+        <h5 class="card-title">Заголовок карточки</h5>
+       <button type="button" class="btn btn-primary">Узнать больше</button>
+      </div>
+
+    </div>
+  </div>
+  <div class="col">
+    <div class="card h-100">
+      <img src="..." class="card-img-top" alt="...">
+      <div class="card-body">
+        <h5 class="card-title">Заголовок карточки</h5>
+        <button type="button" class="btn btn-primary">Узнать больше</button>
+      </div>
+    </div>
+  </div>
+</div>
     </div>
 </main>
+<br>
