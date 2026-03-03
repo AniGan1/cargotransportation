e3c9f62014f4424efa79f06c725db917abcc72e8
student8 <ntti@yandex.ru>
2026-03-03 08:40:17 +0300

    обновил главную

--- application/views/index.php
+++ application/views/index.php
@@ -20,18 +20,18 @@
         <div class="row row-cols-1 row-cols-md-3 g-4">
   <div class="col">
     <div class="card h-100">
-      <img src="..." class="card-img-top" alt="...">
+      <img src="/img/uslug1.jpg" class="card-img-top h-100" alt="...">
       <div class="card-body">
-        <h5 class="card-title">Заголовок карточки</h5>
+        <h5 class="card-title">ПЕРЕВОЗКА ГАЗЕЛЯМИ</h5>
         <button type="button" class="btn btn-primary">Узнать больше</button>
       </div>
     </div>
   </div>
   <div class="col">
     <div class="card h-100">
-      <img src="..." class="card-img-top" alt="...">
+      <img src="/img/uslug2.jpg"  class="card-img-top h-100" alt="...">
       <div class="card-body">
-        <h5 class="card-title">Заголовок карточки</h5>
+        <h5 class="card-title">ПЕРЕВОЗКА ФУРАМИ</h5>
        <button type="button" class="btn btn-primary">Узнать больше</button>
       </div>
 
@@ -39,9 +39,9 @@
   </div>
   <div class="col">
     <div class="card h-100">
-      <img src="..." class="card-img-top" alt="...">
+      <img src="/img/uslug3.jpg" class="card-img-top h-100" alt="...">
       <div class="card-body">
-        <h5 class="card-title">Заголовок карточки</h5>
+        <h5 class="card-title">РЕФРИЖЕРАТОРНАЯ ПЕРЕВОЗКА</h5>
         <button type="button" class="btn btn-primary">Узнать больше</button>
       </div>
     </div>
