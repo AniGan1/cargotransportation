934a49e3462e0c6e6a8d2d7083470740d5a83e13
Konstantin <91148529+AniGan1@users.noreply.github.com>
2026-03-04 13:06:45 +0300

    added manager user

--- application/models/Drivers.php
+++ application/models/Drivers.php
@@ -5,7 +5,24 @@ class Drivers extends CI_Model
 {
     public function selects()
     {
-        $sql1 = "SELECT * FROM driverss";
+        $sql1 = "SELECT * FROM drivers";
+        $result = $this->db->query($sql1);
+        return $result->result_array();
+    }
+
+        public function selectsBuch()
+    {
+        $sql1 = "SELECT  
+    drivers.fio,
+    COUNT(travel_list.id_travel_list) as total_trips, 
+    SUM(applications.total_cost) as total_sum, 
+    SUM(travel_list.transported) as total_transported, 
+    SUM(applications.distance) as total_distance 
+FROM drivers
+LEFT JOIN travel_list ON travel_list.id_driver = drivers.id_driver
+LEFT JOIN applications ON applications.id_application = travel_list.id_application
+LEFT JOIN contracts ON contracts.id_application = applications.id_application
+GROUP BY drivers.fio;";
         $result = $this->db->query($sql1);
         return $result->result_array();
     }
--- application/models/Travel_list.php
+++ application/models/Travel_list.php
@@ -9,4 +9,41 @@ class Travel_list extends CI_Model
         $result = $this->db->query($sql1);
         return $result->result_array();
     }
+
+    public function selectsBuch()
+    {
+        $sql1 = "SELECT DISTINCT travel_list.*, 
+       applications.*, 
+       contracts.*,
+       cars.*,
+       drivers.*
+FROM travel_list
+LEFT JOIN applications ON applications.id_application = travel_list.id_application
+LEFT JOIN contracts ON contracts.id_application = applications.id_application
+LEFT JOIN drivers ON drivers.id_driver = travel_list.id_driver
+LEFT JOIN cars ON cars.id_car = travel_list.id_car
+WHERE travel_list.status_list = 'Завершен' OR travel_list.status_list = 'В пути'  ;";
+        $result = $this->db->query($sql1);
+        return $result->result_array();
+    }
+
+    public function selectsBuchFIlter($date1, $date2, $id_driver)
+    {
+        $sql1 = "SELECT DISTINCT travel_list.*, 
+       applications.*, 
+       contracts.*,
+       cars.*,
+       drivers.*
+FROM travel_list
+LEFT JOIN applications ON applications.id_application = travel_list.id_application
+LEFT JOIN contracts ON contracts.id_application = applications.id_application
+LEFT JOIN drivers ON drivers.id_driver = travel_list.id_driver
+LEFT JOIN cars ON cars.id_car = travel_list.id_car
+WHERE (travel_list.status_list = 'Завершен' OR travel_list.status_list = 'В пути')
+  AND travel_list.dispatch_date >= ? 
+  AND travel_list.the_factual_delivery_date <= ? 
+  AND travel_list.id_driver = ?;";
+        $result = $this->db->query($sql1, array($date1, $date2, $id_driver));
+        return $result->result_array();
+    }
 }
