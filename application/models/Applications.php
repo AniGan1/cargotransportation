45d1d199d6eb518285f34c38d1371ecd331e8433
student8 <ntti@yandex.ru>
2026-03-04 09:39:52 +0300

    Просмотр заявок пользователя, нужно доделать

--- application/models/Applications.php
+++ application/models/Applications.php
@@ -9,4 +9,15 @@ class Applications extends CI_Model
         $result = $this->db->query($sql2, array($data['number_application'], $$data['title_cargo'], $$data['type_cargo'], $data['departure_point'], $data['destination_point'], $data['id_client'], $data['total_cost'], $data['distance']));
         return $result;
     }
+    // Заявки клиента
+    public function client_application(){
+        $sql = "SELECT applications.number_application, applications.title_cargo, type_cargo.title_type_cargo, applications.total_weight, clients.address, applications.total_cost, contracts.status 
+FROM applications, clients, contracts, type_cargo 
+WHERE applications.id_client = clients.id_client 
+AND applications.type_cargo = type_cargo.type_cargo 
+AND applications.id_application = contracts.id_application 
+AND clients.id_client = 1";
+$result = $this->db->query($sql);
+return $result->result_array();
+    }
 }
